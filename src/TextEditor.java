0ea7cd522a067011b796877efd7ce60df5364510
Humaidi01 <humaidikhan1756@gmail.com>
2022-11-12 21:05:11 +0530

    Added new save and open feature

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 
 public class TextEditor implements ActionListener {
     JFrame frame;
@@ -74,12 +75,49 @@ public class TextEditor implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==newFile){
-
+            TextEditor newTextEditor = new TextEditor();
         }
+
         if(e.getSource()==saveFile){
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setApproveButtonText("Save");
+            int chooseOption = fileChooser.showSaveDialog(null);
+            if(chooseOption==JFileChooser.APPROVE_OPTION){
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath()+".txt");
+                String filePath = file.getPath();
+                try{
+                    BufferedWriter outfile = null;
+                    outfile = new BufferedWriter(new FileWriter(file));
+
+                    textArea.write(outfile);
+                    outfile.close();
+                }catch (Exception exception){
+                    System.out.println(exception);
+                }
+            }
 
         }
         if(e.getSource()==openFile){
+            JFileChooser fileChooser = new JFileChooser("C:");
+            int chooseOption = fileChooser.showOpenDialog(null);
+
+            if(chooseOption == JFileChooser.APPROVE_OPTION){
+                File file = fileChooser.getSelectedFile();
+                String filePath = file.getPath();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+                    String intermediate = "", output = "";
+                    while((intermediate = bufferedReader.readLine())!=null){
+                        output += intermediate + "\n";
+                    }
+                    textArea.setText(output);
+                }catch ( Exception exception){
+                    System.out.println(exception);
+                }
+            }
+
+
+
 
         }
         if(e.getSource()==cut){
